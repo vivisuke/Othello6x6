79a5a58267aad3dca9ea34b2d149b340a21a6f84
vivisuke <ntsuda@master.email.ne.jp>
2022-09-17 05:17:22 +0900

    0060 ◎ C 09/17 09/17 0.001│ToDo│盤面タイルマップセル値：黒=1, 白=2 に変更？

--- Godot/Othello6x6/scripts/MainSene.gd
+++ Godot/Othello6x6/scripts/MainSene.gd
@@ -1,12 +1,19 @@
 extends Node2D
 
-const EMPTY = -1
-const BLACK = 1
-const WHITE = 2
+enum {
+	EMPTY = 0, BLACK, WHITE, WALL,
+}
+enum {
+	CAN_PUT = 0, DID_PUT,
+}
+const TRANSPARENT = -1
+#const EMPTY = -1
+#const BLACK = 1
+#const WHITE = 2
 
 func _ready():
-	$Board/TileMap.set_cell(2, 2, WHITE)
-	$Board/TileMap.set_cell(3, 3, WHITE)
-	$Board/TileMap.set_cell(2, 3, BLACK)
-	$Board/TileMap.set_cell(3, 2, BLACK)
+	$Board/TileMap.set_cell(2, 2, WHITE-1)
+	$Board/TileMap.set_cell(3, 3, WHITE-1)
+	$Board/TileMap.set_cell(2, 3, BLACK-1)
+	$Board/TileMap.set_cell(3, 2, BLACK-1)
 	pass # Replace with function body.
