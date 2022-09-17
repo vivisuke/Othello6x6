89be0fc31d3d171848788c7ab04258354e0cc47d
vivisuke <ntsuda@master.email.ne.jp>
2022-09-17 15:34:07 +0900

    0071 ◎ C 09/17 09/17 0.001│ToDo│bd_array[] を原本とし、その状態を TileMap に反映させる

--- Godot/Othello6x6/scripts/MainSene.gd
+++ Godot/Othello6x6/scripts/MainSene.gd
@@ -59,11 +59,43 @@ func _ready():
 	bd_canPutBlack.resize(ARY_SIZE)
 	bd_canPutWhite.resize(ARY_SIZE)
 	#
-	$Board/TileMap.set_cell(2, 2, WHITE-1)
-	$Board/TileMap.set_cell(3, 3, WHITE-1)
-	$Board/TileMap.set_cell(2, 3, BLACK-1)
-	$Board/TileMap.set_cell(3, 2, BLACK-1)
-	pass # Replace with function body.
+	##update_humanAIColor()	# 人間・AI 石色表示
+	init_bd_array()
+	update_TileMap()
+	##update_cursor()
+func xyToArrayIX(x, y):		# 0 <= x, y < 8
+	return (y+1)*ARY_WIDTH + (x+1)
+func aixToX(ix : int):
+	return ix % ARY_WIDTH - 1
+func aixToY(ix : int):
+	return ix / ARY_WIDTH - 1
+func init_bd_array():		#	盤面初期化
+	bd_array.resize(ARY_SIZE)
+	for i in range(ARY_SIZE):
+		bd_array[i] = WALL
+	#print("ARY_SIZE = ", ARY_SIZE)
+	#print("bd_array.size() = ", bd_array.size())
+	for y in range(N_CELL_VERT):
+		for x in range(N_CELL_HORZ):
+			bd_array[xyToArrayIX(x, y)] = EMPTY
+	bd_array[xyToArrayIX(2, 3)] = BLACK
+	bd_array[xyToArrayIX(3, 2)] = BLACK
+	bd_array[xyToArrayIX(2, 2)] = WHITE
+	bd_array[xyToArrayIX(3, 3)] = WHITE
+	next_color = BLACK
+	putIX = 0
+	game_over = false
+func update_TileMap():
+	nColors = [0, 0, 0]		# 空白、黒石、白石数
+	for y in range(N_CELL_VERT):
+		for x in range(N_CELL_HORZ):
+			var col = bd_array[xyToArrayIX(x, y)]
+			$Board/TileMap.set_cell(x, y, col-1)
+			nColors[col] += 1
+	var hix = 1 if AI_color == WHITE else 2
+	var aix = 1 if AI_color == BLACK else 2
+	##$HumanBG/num.text = "%d" % nColors[hix]
+	##$AIBG/num.text = "%d" % nColors[aix]
 
 func _input(event):
 	if event is InputEventMouseButton:
