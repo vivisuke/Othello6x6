680b357ba97113f88a456c9dcbc60fb99b9f8e18
vivisuke <ntsuda@master.email.ne.jp>
2022-09-26 07:58:33 +0900

    0120 ◎ C 09/25 09/26 0.001│ToDo│トップシーン：黒白プレイヤー選択可能に

--- Godot/Othello6x6/scripts/TopScene.gd
+++ Godot/Othello6x6/scripts/TopScene.gd
@@ -6,10 +6,34 @@ var g = Global
 
 func _ready():
 	#var g = get_node("/root/Global")
+	update_black_white_player()
+
+func update_black_white_player():
 	$Black/Human.pressed = g.black_player == g.HUMAN
 	$Black/AI.pressed = g.black_player == g.AI
-	pass # Replace with function body.
+	$White/Human.pressed = g.white_player == g.HUMAN
+	$White/AI.pressed = g.white_player == g.AI
 
 func _on_GameStart_pressed():
 	print("_on_GameStart_pressed()")
 	get_tree().change_scene("res://MainScene.tscn")
+
+
+func _on_Black_Human_toggled(button_pressed):
+	g.black_player = g.HUMAN if button_pressed else g.AI
+	update_black_white_player()
+
+
+func _on_Black_AI_toggled(button_pressed):
+	g.black_player = g.AI if button_pressed else g.HUMAN
+	update_black_white_player()
+
+
+func _on_White_Human_toggled(button_pressed):
+	g.white_player = g.HUMAN if button_pressed else g.AI
+	pass # Replace with function body.
+
+
+func _on_White_AI_toggled(button_pressed):
+	g.white_player = g.AI if button_pressed else g.HUMAN
+	pass # Replace with function body.
